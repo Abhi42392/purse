9cb89530326b9a0f8e22b743c325060b0db37620
Abhinav yadav <Abhi>
2025-11-13 12:53:27 +0530

    Included web analytics component

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ChildLayout from "./ChildLayout";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +25,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
-        <ChildLayout>{children}</ChildLayout>
         <SpeedInsights />
+        <Analytics />
+        <ChildLayout>{children}</ChildLayout>
       </body>
     </html>
   );
